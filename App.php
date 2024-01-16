e226dd9e34138d4887642c35df24f159679feee3
Naoufal <naoufal.labrihmi@gmail.com>
2024-01-16 12:29:46 +0100

    Create App.php

---
+++ App.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+use Classes\Session ;
+use Classes\Request ;
+use Validation\Required ;
+use Validation\Str ;
+use Validation\Validation ;
+use Validation\Validator ;
+
+*/
+
+require_once 'inc/connection.php' ;
+
+require_once 'Classes/Request.php';
+require_once 'Classes/Session.php';
+require_once 'Classes/Validation/Required.php';
+require_once 'Classes/Validation/Str.php';
+require_once 'Classes/Validation/Validation.php';
+require_once 'Classes/Validation/Validator.php';
+
+
+
+
+
+
+
+$request=new Request ;
+$session=new Session ;
+$validation=new Validation ;
