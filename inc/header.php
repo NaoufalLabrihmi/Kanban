9484ccbe835ca84d8692602db88010222f0ea7b9
Naoufal <naoufal.labrihmi@gmail.com>
2024-01-16 12:29:04 +0100

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'inc/connection.php';
+require_once 'App.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        body {
+            background-image: url('../imgs/pexels-vlad-alexandru-popa-1402787.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        /* Add more styles for other elements as needed */
+    </style>
+    <!-- Include Bootstrap JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>ToDO-Kanban</title>
+</head>
+<body>
+<!-- The rest of your HTML content goes here -->
